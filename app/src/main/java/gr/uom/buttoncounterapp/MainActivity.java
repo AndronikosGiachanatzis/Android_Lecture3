a9472e9b3ad9210eaff2035fc4c393bf3c7a252b
AndronikosGiachanatzis <a.giachanatzis@gmail.com>
2020-10-30 15:49:57 +0200

    Overriding basic lifecycle methods

--- app/src/main/java/gr/uom/buttoncounterapp/MainActivity.java
+++ app/src/main/java/gr/uom/buttoncounterapp/MainActivity.java
@@ -1,7 +1,9 @@
 package gr.uom.buttoncounterapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,12 +13,14 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextView textView;
     private int clickCounter = 0;
-
+    private static final String LOG_TAG = "TestLog";
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -35,4 +39,68 @@ public class MainActivity extends AppCompatActivity {
 
         button.setOnClickListener(listener);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(LOG_TAG, "onStart");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(LOG_TAG, "onStop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d(LOG_TAG, "onDestroy");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(LOG_TAG, "onPause");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.d(LOG_TAG, "onRestart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(LOG_TAG, "onResume");
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(LOG_TAG, "onSaveInstanceState");
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(LOG_TAG, "onRestoreInstanceState");
+
+    }
+
+
 }
